a68866d6f89ae8fa6867a2bebd54ff721eba0a9a
K4voos <55686856+K4voos@users.noreply.github.com>
2021-03-21 14:59:40 +0330

    basic logit wrote

---
+++ Main.py
@@ -0,0 +1,17 @@
+def average_entry_price():
+    total_amount = 0
+    average = 0
+    while True:
+        entry_amount = float(input("How many shares have you bought? (-1 to skip): "))
+        if entry_amount == -1:
+            break
+        total_amount += entry_amount
+        entry_price = float(input("At what price you've bought your shares? "))
+        average += entry_price * entry_amount
+    average /= total_amount
+    return average
+
+average = average_entry_price()
+print('\nYour average entry price is: {} dollars.'.format(average))
+
+input('\nPress Enter to exit.')
