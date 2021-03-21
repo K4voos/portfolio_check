fd5f31de7735cbf392b91e1093bdce03f6d6f9d1
K4voos <55686856+K4voos@users.noreply.github.com>
2021-03-21 15:52:58 +0330

    get_input() and calculate_average() methods separated

--- Main.py
+++ Main.py
@@ -1,18 +1,33 @@
-def average_entry_price():
-    total_amount = 0
-    average = 0
+def get_input():
+    entry_history = {}
+
     while True:
-        entry_amount = float(input("How many shares have you bought? (-1 to skip): "))
-        if entry_amount == -1:
+        buy_amount = float(input("How many shares have you bought? (-1 to skip): "))
+        if buy_amount == -1:
             break
-        total_amount += entry_amount
-        entry_price = float(input("At what price you've bought your shares? "))
-        average += entry_price * entry_amount
-    average /= total_amount
-    return average
+        buy_price = float(input("At what price you've bought your shares? "))
+        if buy_price in entry_history:
+            entry_history[buy_price] += buy_amount
+        else:
+            entry_history[buy_price] = buy_amount
+    return entry_history
+    #     total_amount += buy_amount
+    #     ave += buy_price * buy_amount
+    # ave /= total_amount
+    # return ave
 
 
-this_average = average_entry_price()
+def calculate_average(dictionary):
+    total_amount = 0
+    ave = 0
+    for key in dictionary:
+        ave += key * dictionary[key]
+        total_amount += dictionary[key]
+    ave /= total_amount
+    return ave
+
+
+this_average = calculate_average(get_input())
 print('\nYour average entry price is: {} dollars.'.format(this_average))
 
-input('\nPress Enter to exit.')
+# input('\nPress Enter to exit.')
