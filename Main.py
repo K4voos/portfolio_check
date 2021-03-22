61b4d2155df16936bf913dfd0caa6adcedbe147b
K4voos <55686856+K4voos@users.noreply.github.com>
2021-03-23 01:47:17 +0430

    write method created and working for one time use.

--- Main.py
+++ Main.py
@@ -1,6 +1,9 @@
+import csv
+
+# currency_name = ''
 def get_input():
     entry_history = {}
-
+    # currency_name = input("Name the currency you want to check: ")
     while True:
         buy_amount = float(input("How many shares have you bought? (-1 to skip): "))
         if buy_amount == -1:
@@ -13,6 +16,14 @@ def get_input():
     return entry_history
 
 
+def save_to_file(dictionary):
+    f = open("Portfolio_Checker/entry.csv", "w", newline="")
+    writer = csv.writer(f)
+    writer.writerow(("price", "amount"))
+    for key in dictionary:
+        writer.writerow((key, dictionary[key]))
+    f.close()
+
 
 def calculate_average(dictionary):
     total_amount = 0
@@ -24,7 +35,10 @@ def calculate_average(dictionary):
     return ave
 
 
-this_average = calculate_average(get_input())
+this_input = get_input()
+
+this_average = calculate_average(this_input)
+save_to_file(this_input)
 print('\nYour average entry price is: {} dollars.'.format(this_average))
 
 # input('\nPress Enter to exit.')
