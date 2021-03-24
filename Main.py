b0df96f5089b0fc4a4bd9230a3c1e9f676ff81b3
K4voos <55686856+K4voos@users.noreply.github.com>
2021-03-25 03:26:35 +0430

    user now chooses to either open an existing file or start a new operation.

--- Main.py
+++ Main.py
@@ -1,18 +1,40 @@
 import csv
+from tkinter import filedialog
 
 
 def get_input():
     entry_history = {}
-    while True:
-        buy_amount = float(input("How many shares have you bought? (-1 to skip): "))
-        if buy_amount == -1:
-            break
-        buy_price = float(input("At what price you've bought your shares? "))
-        if buy_price in entry_history:
-            entry_history[buy_price] += buy_amount
-        else:
-            entry_history[buy_price] = buy_amount
+    init = input("Press 'o' to open an existing file, or anything else for new operation: ")
+
+    if init == 'o':
+        entry_history = open_file()
+    else:
+        while True:
+            buy_amount = float(input("How many shares have you bought? (-1 to skip): "))
+            if buy_amount == -1:
+                break
+            buy_price = float(input("At what price you've bought your shares? "))
+            if buy_price in entry_history:
+                entry_history[buy_price] += buy_amount
+            else:
+                entry_history[buy_price] = buy_amount
+
+    print(entry_history)
     return entry_history
+
+
+def open_file():
+    entry = {}
+    file_name = filedialog.askopenfilename(initialdir="/", title="Select File",
+                                           filetypes=(("SCV files", "*.csv"), ("all files", "*.*")))
+    f = open(file_name, 'r')
+    csv_reader = csv.reader(f)
+    next(csv_reader)
+    for line in csv_reader:
+        entry[float(line[0])] = float(line[1])
+    f.close()
+
+    return entry
 
 
 def save_to_file(dictionary):
